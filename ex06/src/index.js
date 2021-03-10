04c9eb337c56949100056b2656f71b2c03e7d8b1
Majra <majra.oruc@gmail.com>
2021-03-10 22:04:05 +0100

    ex05 three variables

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //Only change code below this line
+    var x=9;
+    var y=16;
+    var z="My name is";
+    //Only change code above this line
+    x=x+2;
+    y=y+4;
+    z=z+ " John!";
+}
+console.log(main());
+module.exports = main;
