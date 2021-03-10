fa773333ee316da2341e656a3b33404ca34d686c
Majra <majra.oruc@gmail.com>
2021-03-10 23:37:22 +0100

    Product of two numbers

---
+++ ex10/src/index.js
@@ -0,0 +1,8 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    var product = numOne * numTwo;
+    //Only change code above this line
+    return product;
+}
+console.log(main(15, 3));
+module.exports = main;
