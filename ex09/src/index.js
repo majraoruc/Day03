a132f6dfcdd3b479879c3761818f287fb7416fe8
Majra <majra.oruc@gmail.com>
2021-03-10 22:21:35 +0100

    difference of two numbers

---
+++ ex09/src/index.js
@@ -0,0 +1,8 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    var difference = numOne - numTwo;
+    //Only change code above this line
+    return difference;
+}
+console.log(main(numOne, numTwo));
+module.exports = main;
