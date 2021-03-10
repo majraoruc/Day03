8ec4dba0c97c8d53e7f7d86317d335335360be49
Majra <majra.oruc@gmail.com>
2021-03-10 21:32:06 +0100

    ex02 comments

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    //Only change code below this line
+    //This is a single line JavaScript comment
+    /* document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+     */
+    //Only change code above this line
+    
+}
+console.log(main());
+
+module.exports = main;
