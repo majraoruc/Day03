2a802d1c721c8ecb864b44e3b8a375906494a3e4
Majra <majra.oruc@gmail.com>
2021-03-10 22:17:59 +0100

    ex08 sum of two numbers

---
+++ ex08/src/index.js
@@ -0,0 +1,8 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+   var sum = numOne + numTwo;
+    //Only change code above this line
+    return sum;
+}
+console.log(main(numOne, numTwo));
+module.exports = main;
